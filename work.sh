55d8a211366063887ad3d91a7c20075f00125418
Zhenyun Yu <yzyDavid@qq.com>
2018-01-16 11:55:19 +0800

    a version of pull, replacing static

--- work.sh
+++ work.sh
@@ -8,42 +8,19 @@ repo_url="https://github.com/yzyDavid/blog.yuzhenyun.me.git"
 deploy_dir="/var/www/hexoblog"
 repo_name="blog.yuzhenyun.me"
 
-echo "working directory: $(pwd)"
-echo "cloning from $repo_url"
-git clone $repo_url
-if [ $? -eq 0 ]; then
-    echo "clone finished"
-else
-    echo "clone failed, exit!"
-    exit 1
-fi
-
-echo "generating static files"
+echo "pulling upstream"
 cd $repo_name
 echo "working directory: $(pwd)"
-yarn add -D hexo
-yarn install
-hexo g
-if [ $? -eq 0 ]; then
-    echo "generation finished"
-else
-    echo "generation failed, exit!"
-    exit 1
-fi
+git pull
 
 echo "replacing"
 rm -rf $deploy_dir
+cp -r "public" $deploy_dir
 if [ $? -eq 0 ]; then
     echo "replacement finished"
 else
     echo "replacement failed, exit!"
     exit 1
 fi
-mv "public" $deploy_dir
-
-echo "cleaning"
-cd ".."
-echo "working directory: $(pwd)"
-rm -rf $repo_name
 
 echo "finished!"
